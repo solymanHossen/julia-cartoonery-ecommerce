16c78cad5815e2415a39b8b1032fcecdc5f8b0cf
Md Solyman Hossen <105260985+solymanHossen@users.noreply.github.com>
2026-05-01 22:42:07 +0600

    chore: update Tailwind CSS base styles and cleanup CSS output

--- functions.php
+++ functions.php
@@ -3,10 +3,12 @@
  * Julia's Cartoonery Theme Functions
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 
-require_once get_theme_file_path( 'inc/theme-setup.php' );
-require_once get_theme_file_path( 'inc/enqueue-scripts.php' );
-require_once get_theme_file_path( 'inc/post-types.php' );
+require_once get_theme_file_path('inc/theme-setup.php');
+require_once get_theme_file_path('inc/enqueue-scripts.php');
+require_once get_theme_file_path('inc/post-types.php');
+require_once get_theme_file_path('inc/menu-setup.php');
+
