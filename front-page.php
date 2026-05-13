5ea04583deadafe6fdae8c00fd34af3ec6ddc51f
Solyman Hossen <solyman@mediusware.com>
2026-05-13 15:14:59 +0600

    refactor: reorder reviews section in front-page template

--- front-page.php
+++ front-page.php
@@ -3,9 +3,9 @@
 <main>
 	<?php get_template_part('template-parts/hero', 'carousel'); ?>
 	<?php get_template_part( 'template-parts/sections/latest-products', 'section' ); ?>
-	<?php get_template_part( 'template-parts/reviews-section' ); ?>
 	<?php get_template_part( 'template-parts/sections/favorite-toys', 'section' ); ?>
 	<?php get_template_part('template-parts/sections/videos', 'section'); ?>
+	<?php get_template_part( 'template-parts/reviews-section' ); ?>
 </main>
 
 <?php get_footer(); ?>
